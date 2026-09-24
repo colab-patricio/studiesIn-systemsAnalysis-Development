5dbb5972472ede043225ac3be20c20389367b2b5
Alexandre Patricio <patricioalexandredeot@gmail.com>
2026-09-24 20:15:40 -0300

    Fix Q5AF.py

--- Graduação/1st_semester/logicaProgramacao_algoritmos/AF/Q5AF.py
+++ Graduação/1st_semester/logicaProgramacao_algoritmos/AF/Q5AF.py
@@ -11,5 +11,5 @@ numero = int(input("Digite um número inteiro: "))
 # Solução por estrutura condicional
 if numero % 2 == 0:
     print("O número é par.")
-else
+else:
     print("O número é ímpar.")
