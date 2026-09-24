c3abeffe3f2d2f0705b8154434a01782f39e6659
Alexandre Patricio <patricioalexandredeot@gmail.com>
2026-09-24 18:58:35 -0300

    Update directorys

---
+++ Graduação/1st_semester/logicaProgramacao_algoritmos/Listas de exercícios/Lista 1/Q1L1.py
@@ -0,0 +1,7 @@
+'''1. Faça um programa que receba dois números, calcule e mostre a subtração do primeiro
+número pelo segundo.'''
+
+num1 = float(input("Digite um número: "))
+num2 = float(input("Digite um segundo número: "))
+
+print(f"\nA subtração do primeiro pelo segundo número é: {num1-num2}")
